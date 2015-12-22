56baa488a82ffed0dd03ea1352471bc9ac9050c5
Josh Cain <jcain@redhat.com>
2015-12-22 08:45:23 -0600

    Add menu bar entry to launch extension

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var self = require('sdk/self');
 
+var buttons = require('sdk/ui/button/action');
+var tabs = require('sdk/tabs');
+
+var button = buttons.ActionButton({
+  id: "authInspector",
+  label: "Auth Inspector",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onClick: openGithubTab
+})
+
+function openGithubTab(state) {
+    tabs.open("https://github.com/cainj13/authInspector");
+}
+
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
 function dummy(text, callback) {
