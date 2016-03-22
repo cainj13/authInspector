1083d7c058265c7895efd226e7a199c7225c4e57
Josh Cain <jcain@redhat.com>
2016-03-22 08:50:28 -0500

    add text popup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # authInspector
 Firefox extension for debugging commonly used authn/authz protocols
+
+	jpm run -b /usr/bin/firefox
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var self = require('sdk/self');
+var data = require("sdk/self").data;
 
 var buttons = require('sdk/ui/button/action');
 var tabs = require('sdk/tabs');
+
+// Create a panel for displaying text
+var text_entry = require("sdk/panel").Panel({
+  contentURL: data.url("text-entry.html"),
+  contentScriptFile: data.url("get-text.js")
+});
 
 var button = buttons.ActionButton({
   id: "authInspector",
@@ -11,17 +18,33 @@ var button = buttons.ActionButton({
     "32": "./icon-32.png",
     "64": "./icon-64.png"
   },
-  onClick: openGithubTab
+  //onClick: openGithubTab
+  onClick: showText
 })
 
+// demonstrates how to open a link
 function openGithubTab(state) {
     tabs.open("https://github.com/cainj13/authInspector");
 }
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+// Show the panel when the user clicks the button.
+function showText(state) {
+  text_entry.show();
 }
 
-exports.dummy = dummy;
+// When the panel is displayed it generated an event called
+// "show": we will listen for that event and when it happens,
+// send our own "show" event to the panel's script, so the
+// script can prepare the panel for display.
+text_entry.on("show", function() {
+  text_entry.port.emit("show");
+});
+
+// Listen for messages called "text-entered" coming from
+// the content script. The message payload is the text the user
+// entered.
+// In this implementation we'll just log the text to the console.
+text_entry.port.on("text-entered", function (text) {
+  console.log(text);
+  text_entry.hide();
+});
